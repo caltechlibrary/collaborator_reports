c079c47549c475b37d3ec4c929f16bf53a36da4d
Tom Morrell <tmorrell@caltech.edu>
2018-05-03 15:59:30 -0700

    First version of NIH training colaborator report

--- nih_training.py
+++ nih_training.py
@@ -5,20 +5,24 @@ import ads
 import dataset
 from clint.textui import progress
 
-
 path="CaltechAUTHORS.ds"
 
 #subprocess.run(['dataset',path,'indexer','authors.json','authors.bleve'])
 
-authors = ['Anderson-D-J','Aravin-A-A','Arnold-F-H']
+#authors = ['Anderson-D-J','Aravin-A-A','Arnold-F-H']
+
+authors = []
+infile = open('authors.txt','r')
+for line in infile:
+    authors.append(line.strip())
 
 #Eprints records with co-authored papers
 records = []
 
 for a in authors:
+    print(a)
     response =\
         subprocess.check_output(['dataset','-json','-size','10000','find','authors.bleve','authors:'+a],universal_newlines=True)
-    print(response)
     response = json.loads(response)
     keys = []
     for h in response['hits']:
@@ -26,8 +30,6 @@ for a in authors:
     count = 0
     for k in keys:
         #print(k)
-        count = count + 1
-        print(count)
         metadata = subprocess.check_output(["dataset","-c",path,"read",str(k)],universal_newlines=True)
         metadata = json.loads(metadata)
         #Pull out date
@@ -53,9 +55,26 @@ for a in authors:
                 author_ids = []
                 for c in metadata['creators']:
                     author_ids.append(c['id'])
-                print('COMP',authors,author_ids)
-                print(set(authors).intersection(author_ids))
+                #print('COMP',authors,author_ids)
+                #print(set(authors).intersection(author_ids))
+                match = set(authors).intersection(author_ids)
+                if len(match) > 1:
+                    print('Yes!')
+                    records.append(metadata)
         else:
             print("Record "+str(k)+"does not have date, discarding")
 
-
+ids = []
+for r in records:
+    idv = r['id']
+    if idv not in ids:
+        ids.append(idv)
+        citation = ''
+        for c in r['creators']:
+            citation = citation + c['family'] +', '+ c['given'] +'; '
+        citation = citation + ' (' + r['date'].split('-')[0] + ') '
+        citation = citation + r['title'] + '.'
+        if 'publication' in r:
+            citation = citation + ' ' + r['publication']
+        citation = citation + ' ' + r['official_url']
+        print(citation)
