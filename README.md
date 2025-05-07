ad529638c27bd3dbabd497ffac1442b129460308
Tom Morrell <tmorrell@caltech.edu>
2025-05-07 12:30:06 -0700

    New authors NSF collaborator report

--- README.md
+++ README.md
@@ -7,20 +7,25 @@ various data sources.
 
 Requires: 
 
-Python 3 (Recommended via [Anaconda](https://www.anaconda.com/download)) with reqests library and [Dataset](https://github.com/caltechlibrary/dataset).
+Python 3 and the requirements defined in the requirements.txt file (e.g. `pip install -r /path/to/requirements.txt`).
 
-## Usage
+## NSF Collaborator Report
 
-### WOS-based Report
-Get a WOS API token from https://developer.clarivate.com/.  Open a terminal window and save your token by typing `export WOSTOK='TOKEN"`, replacing TOKEN with the actual token.
-Search on Web of Science to determine the correct author search term (e.g. Mooley K).
-Open a new google sheet and copy the sheet ID (the long random character string between the end /'s in the sheet url). 
-Type `python get_articles_wos.py`. 
-The script will request the WOS author search term, whether you want to restrict your search to Caltech, and the google sheet id where you want the results to be output. Review this sheet to ensure each article is correct.
-This script generates a dataset collection using the last name in the search term (e.g. Mooley.ds)
+Find the identifier of the individual you want to run a report on. This can be
+a Caltech Library person identifier (listed at
+https://feeds.library.caltech.edu/people/) or an ORCID identifier.
 
-## Formatting NSF_C_A Report
-Once you have reviewed the articles, type `python format_nsf_c_a.py dataset_collection input_google_sheet_id output_google_sheet_id`, where the dataset_collection is from the get_articles step, the input_google sheet is the reviewed spreadsheet with articles, and output_google_sheet is the new sheet where the report fragment will be generated.  An additional option `-limited` limits the results to the first three authore per paper.  This formatting script de-duplicates the authors and generates a formatted report.
+Type the following command in the terminal:
+
+```bash
+python authors_nsf_table4.py <identifier>
+```
+
+Where `<identifier>` is the identifier of the individual you want to run a report
+
+This will generate a file nsf_collaborator_report.xlsx with the contents of
+table 4 part A
 
 ## Additional sources
-Additional scripts in this repository for ADS and CaltechAUTHORS are from earlier attempts and will all be merged into the above harvest and format approach.
+Additional scripts in this repository for ADS and WOS are using an older
+approach and may or may not work. 
---
+++ authors_nsf_table4.py
@@ -0,0 +1,146 @@
+import argparse
+import openpyxl
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from ames.harvesters import get_author_records
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
+
+parser = argparse.ArgumentParser(
+    prog="nsf_c_s",
+    description="Harvests content from CaltechAUTHORS for a NSF formetted collaborator report",
+)
+parser.add_argument(
+    "author_identifier", type=str, help="The old identifier to be replaced"
+)
+
+args = parser.parse_args()
+author_identifier = args.author_identifier
+
+# Get the current date
+current_date = datetime.now()
+
+# Need publications within the last 48 months
+start_date = current_date - relativedelta(months=48)
+start_date = start_date.strftime("%Y-%m-%d")
+
+records = get_author_records(
+    author_identifier=author_identifier,
+    date=start_date,
+    all_metadata=True
+)
+
+def update_coauthor(coauthor,new_author_info,year):
+    # update year
+    if coauthor['year'] < year:
+        coauthor['year'] = year
+    # add affiliation
+    if 'affiiations' in new_author_info:
+        if new_author_info['affiliations'] not in coauthor['affiliations']:
+            coauthor['affiliations'].append(new_author_info['affiliations'])
+
+def create_coauthor(author, year):
+    if "affiliations" in author:
+        affiliations = author["affiliations"]
+    else:
+        affiliations = []
+    return {
+        "name": author["person_or_org"]["name"],
+        "affiliations": affiliations,
+        "year": year,
+    }
+
+coauthors = {}
+
+
+for article in records:
+    year = article["metadata"]["publication_date"].split("-")[0]
+    authors = article['metadata']['creators']
+    for author in authors:
+        name = author["person_or_org"]["name"]
+        print(name)
+        if "identifiers" in author["person_or_org"]:
+            identifiers = author["person_or_org"]["identifiers"]
+            clpid = None
+            orcid = None
+            for identifier in identifiers:
+                if identifier["scheme"] == 'clpid':
+                    clpid = identifier["identifier"]
+                if identifier["scheme"] == 'orcid':
+                    orcid = identifier["identifier"]
+            print(clpid,orcid)
+            if clpid:
+                print('CLPID')
+                if clpid in coauthors:
+                    print('update')
+                    coauthor = coauthors[clpid]
+                    update_coauthor(coauthor,author,year)
+                elif orcid:
+                    if orcid in coauthors:
+                        #If the orcid record got created first, use that
+                            print('update')
+                            coauthor = coauthors[orcid]
+                            update_coauthor(coauthor,author,year)
+                    else:
+                        print('new-clpid')
+                        coauthors[clpid] = create_coauthor(author, year)
+                else:
+                    print('new-clpid')
+                    coauthors[clpid] = create_coauthor(author, year)
+            elif orcid:
+                print('ORCID')
+                if orcid in coauthors:
+                    print('update')
+                    coauthor = coauthors[author_identifier]
+                    update_coauthor(coauthor,author,year)
+                else:
+                    print('new-orcid')
+                    coauthors[orcid] = create_coauthor(author, year)
+        else:
+            if name in coauthors:
+                print('update')
+                coauthor = coauthors[name]
+                update_coauthor(coauthor,author,year)
+            else:
+                print('new-name')
+                coauthors[name] = create_coauthor(author, year)
+
+# Headers for the NSF collaborator report
+header = ["4", "Name:", "Organizational Affiliation", "Optional (email, Department", "Last Active"]
+data = []
+# Add coauthors to the data list
+for coauthor in coauthors:
+    a_string = ''
+    for affiliation in coauthors[coauthor]["affiliations"]:
+        if a_string == '':
+            a_string = affiliation['name']
+        else:
+            a_string += f", {affiliation['name']} "
+    data.append(["A:", coauthors[coauthor]["name"], a_string, "", coauthors[coauthor]["year"]])
+
+sorted_rows = sorted(data, key=lambda x: x[1])
+
+# Create a new Excel workbook
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+sheet.title = "NSF Collaborator Report Table 4"
+
+# Populate the sheet with data
+for row_index, row_data in enumerate([header] + sorted_rows, start=1):
+    for col_index, cell_value in enumerate(row_data, start=1):
+        cell = sheet.cell(row=row_index, column=col_index, value=cell_value)
+
+        # Apply bold font to header row
+        if row_index == 1:
+            cell.font = Font(bold=True)
+
+# Adjust column widths for better readability
+for col_index in range(1, len(data[0]) + 1):
+    column_letter = get_column_letter(col_index)
+    sheet.column_dimensions[column_letter].width = 20
+
+# Save the workbook to a file
+output_filename = "nsf_collaborator_report.xlsx"
+workbook.save(output_filename)
+
+print(f"Report saved to {output_filename}")
